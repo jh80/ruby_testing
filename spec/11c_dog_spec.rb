6d9503af15f974e1485ff3c5c3acff28be22b1d0
Jennifer H <74622778+jh80@users.noreply.github.com>
2025-06-01 11:48:09 -0700

    Complete Pet Dog assignment

--- spec/11c_dog_spec.rb
+++ spec/11c_dog_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Dog do
   # Create a subject with your choice of dog name and optional breed/color.
-
+  subject(:milo) { described_class.new("Milo", "Lab", "Brown")}
   # Write a test using the second shared_example to test that dog responds to
   # talk ('WOOF!').
-  context '' do
+  context 'when Cat and Dog are functionaly similar classes' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not barking' do
+  it 'is not barking' do
+    expect(milo.barking?).to be(false)
   end
 
   # remove the 'x' before running this test
-  xit 'is sleeping' do
+  it 'is sleeping' do
+    expect(milo.sleeping?).to eq(true)
   end
 end
