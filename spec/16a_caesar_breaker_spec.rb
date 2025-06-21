16b00202e254e7e707ad05789a002522b7af0f17
Jennifer H <74622778+jh80@users.noreply.github.com>
2025-06-21 12:15:39 -0700

    Complete ceasar breaker assignments #1-#4

--- spec/16a_caesar_breaker_spec.rb
+++ spec/16a_caesar_breaker_spec.rb
@@ -42,9 +42,16 @@ describe CaesarBreaker do
   describe '#create_decrypted_messages' do
     # This is a Looping Script Method.
     # Located inside #decrypt (Public Script Method)
+  
+    # I CAN'T TELL IF THIS  IS NECESSARY, THE TEST PASSES WITH OUT IT, BUT MAYBE IT IS A BAD TEST
+    before do
+      #allow(translator).to receive(:translate)
+    end
 
     # Method with Outgoing Command -> Test that a message is sent
-    xit 'sends translate 26 times' do
+    it 'sends translate 26 times' do
+      expect(translator).to receive(:translate).exactly(26).times
+      phrase.create_decrypted_messages
     end
   end
 
@@ -79,14 +86,28 @@ describe CaesarBreaker do
       # ASSIGNMENT #2
       # Write the following 3 tests:
 
-      xit 'sends message to check the existence of the 16_cipher directory' do
+      it 'sends message to check the existence of the 16_cipher directory' do
+        expect(Dir).to receive(:exist?).with('16_cipher').once
+        phrase.save_decrypted_messages
       end
 
-      xit 'sends message to create a directory' do
+      it 'sends message to create a directory' do
+        expect(Dir).to receive(:mkdir).once
+        phrase.save_decrypted_messages
       end
 
-      xit 'sends message to create a file' do
+      it 'sends message to create a file' do
+        expect(File).to receive(:open).once
+        phrase.save_decrypted_messages
       end
+
+      # I don't feel comfortable enough with after block to use this, but
+      # when I commented out phrase.save_decrypted_message from the examples
+      # it worked
+      # 
+      # after do
+      #   phrase.save_decrypted_messages
+      # end
     end
 
     # ASSIGNMENT #3
@@ -95,15 +116,25 @@ describe CaesarBreaker do
     # Method with Outgoing Commands -> Test that the messages are sent
     context 'when the directory exists' do
       before do
+        allow(Dir).to receive(:exist?).and_return(true)
+        allow(Dir).to receive(:mkdir)
+        allow(File).to receive(:open)
+        allow(phrase).to receive(:display_file_location)
       end
 
-      xit 'sends message to check the existence of the 16_cipher directory' do
+      it 'sends message to check the existence of the 16_cipher directory' do
+        expect(Dir).to receive(:exist?).with('16_cipher').once
+        phrase.save_decrypted_messages
       end
 
-      xit 'does not send message to create a directory' do
+      it 'does not send message to create a directory' do
+        expect(Dir).not_to receive(:mkdir)
+        phrase.save_decrypted_messages
       end
 
-      xit 'sends message to create a file' do
+      it 'sends message to create a file' do
+        expect(File).to receive(:open)
+        phrase.save_decrypted_messages
       end
     end
 
@@ -157,7 +188,9 @@ describe CaesarBreaker do
   describe '#save_to_yaml' do
     # Method with Outgoing Command -> Test that a message is sent
 
-    xit 'dumps to yaml' do
+    it 'dumps to yaml' do
+      expect(YAML).to receive(:dump)
+      phrase.save_to_yaml
     end
   end
 end
